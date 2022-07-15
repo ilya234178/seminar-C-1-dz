a6e90f68c26ea2eac1b750c7c6e19bce477ce400
Ilya Veryuhanov <veruhanov00@mail.ru>
2022-07-15 14:22:22 +0400

    add task 13.2

--- task13/Program.cs
+++ task13/Program.cs
@@ -16,3 +16,24 @@ if (anyNumberText.Length > 2){
 else {
   Console.WriteLine("-> третьей цифры нет");
 }
+
+// выводим второе число
+
+// Console.Clear(); // очистка терминала
+// Console.WriteLine("Введите трехзначное число: "); // запрашивает 3 числа
+// int numberOne = Convert.ToInt32(Console.ReadLine()); // поле для ввода
+// double SecondDigit(int number) //присваеваем 2 числу переменную
+// {
+//   double resultSecond = 1; // присваеваем в случае, ввода меньше 3 чисел
+//   if (number < 100 || number > 999) // проверка на ввод 3 числе
+//   {
+//     Console.WriteLine("Error: Введите трехзначное число"); // вывод в случае, если ввел меньше 
+//   }
+//   else{
+//     double result = number / 10; //сдвигаемся ко 2 целому числучислу
+//     resultSecond = Math.Round(result, 1) % 10; 
+//   }
+//   return resultSecond; // возвращаем нужное число
+// }
+// Console.WriteLine(SecondDigit(numberOne)); // выводим второе число
+// Переделать как разберешься
